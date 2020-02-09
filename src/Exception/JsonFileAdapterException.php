4668df6f2d2ab776ec2ddea0ab0a0a397aeaad35
Byron Adams <byron.adams54@gmail.com>
2020-02-10 08:44:51 +1300

    Adding license doc-blocks

--- src/Exception/JsonFileAdapterException.php
+++ src/Exception/JsonFileAdapterException.php
@@ -1,8 +1,17 @@
 <?php
 
+/*
+ * This file is part of the secrecy/secrecy package.
+ *
+ * (c) Webtools Ltd <support@webtools.co.nz>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
 
 namespace Secrecy\Exception;
-
 
 class JsonFileAdapterException extends \Exception implements ExceptionInterface
 {
